e58c0c5c7d88b6507bd9b9144bd28e01c285a734
hanago <a456456417@gmail.com>
2015-09-28 00:11:12 +0800

    Compute Fibonacci number by  recursive method

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -1,43 +1,51 @@
-	.syntax unified
-	.arch armv7-a
-	.text
-	.align 2
-	.thumb
-	.thumb_func
+    .syntax unified
+    .arch armv7-a
+    .text
+    .align 2
+    .thumb
+    .thumb_func
 
-	.global fibonacci
-	.type fibonacci, function
+    .global fibonacci
+    .type fibonacci, function
 
 fibonacci:
-	@ ADD/MODIFY CODE BELOW
-	@ PROLOG
-	push {r3, r4, r5, lr}
+    @ ADD/MODIFY CODE BELOW
+    @ PROLOG
+    push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+    
+    @ if(x <= 0) retrun 0;
+    cmp r0, #0
+    ble .L3
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+    @ else if(x <= 2) retrun 1;
+    cmp r0, #2 
+    ble .L4 
+    
+    @ temp = x
+    mov r3, r0
+    
+    @ a = fibonacci(temp - 1);
+    subs r0, r3, #1
+    bl fibonacci
+    mov r4, r0
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
+    @ b = fibonacci(temp - 2);
+    subs r0, r3, #2
+    bl fibonacci 
+    
+    @ return a+b;
+    adds r0, r0, r4
+    pop {r3, r4, r5, pc}        @EPILOG
 
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
-
-	@ END CODE MODIFICATION
+    @ END CODE MODIFICATION
 .L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
+    mov r0, #0            @ R0 = 0
+    pop {r3, r4, r5, pc}        @ EPILOG
 
 .L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+    mov r0, #1            @ R0 = 1
+    pop {r3, r4, r5, pc}        @ EPILOG
 
-	.size fibonacci, .-fibonacci
-	.end
+    .size fibonacci, .-fibonacci
+    .end
